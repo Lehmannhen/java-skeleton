6539cdc2e8e1e92bd68710492710319c3df20d39
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-18 21:37:07 +0100

    Question 1 completed

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,16 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+        int maxMergedPort = -1;
+        int tempMergedPort;
+
+        Arrays.sort(portfolios);
+        for (int i = 0; i < portfolios.length - 1; i++) {
+            tempMergedPort = portfolios[i] ^ portfolios[i + 1];
+            if (tempMergedPort > maxMergedPort)
+                maxMergedPort = tempMergedPort;
+        }
+        return maxMergedPort;
 	}
 
 }
