0177e90c85e09361eb9f94349bc1edc864c86a03
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-20 18:33:58 +0100

    Fixed so that all portfolios are compared with each other

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -6,12 +6,15 @@ public class Question1 {
     public static int bestMergedPortfolio(int[] portfolios) {
         int maxMergedPort = -1;
         int tempMergedPort;
+        int n = portfolios.length;
 
         Arrays.sort(portfolios);
-        for (int i = 0; i < portfolios.length - 1; i++) {
-            tempMergedPort = portfolios[i] ^ portfolios[i + 1];
-            if (tempMergedPort > maxMergedPort)
-                maxMergedPort = tempMergedPort;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                tempMergedPort = portfolios[i] ^ portfolios[j];
+                if (tempMergedPort > maxMergedPort)
+                    maxMergedPort = tempMergedPort;
+            }
         }
         return maxMergedPort;
     }
