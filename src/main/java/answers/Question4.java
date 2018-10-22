676968f5b09cc3637b0a81dfb2518beceafc8614
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-22 14:25:31 +0100

    Bug fixes

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -1,18 +1,21 @@
 package answers;
+import java.lang.Math;
 
 public class Question4 {
 
     public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-        int minTime = 100000000;
-        int tempTime;
+        int minTime = Integer.MAX_VALUE;
+        int tempTime = 0;
+        boolean isNonZero = false;
 
         for (int i = 0; i < rows.length; i++) {
             tempTime = selectionFailedTradedesks(rows[i], numberMachines);
-            if (tempTime != 0)
-                if (minTime > tempTime)
-                    minTime = tempTime;
+            if (tempTime > 0) {
+                minTime = Math.min(minTime, tempTime);
+                isNonZero = true;
+            }
         }
-        if (minTime == 100000000)
+        if (!isNonZero)
             return 0;
 
         return minTime;
@@ -21,44 +24,50 @@ public class Question4 {
 
 
     public static int selectionFailedTradedesks(String[] row, int numberMachines) {
-        int[] consecMachineSums = new int[row.length];
+        int[] consecMachineSums = new int[row.length + 1];
+
         int minTime;
         int newTime;
         int startingPoint = 0;
         consecMachineSums[0] = 0;
         int numTraders = row.length;
 
-        if (subsecKSum(consecMachineSums,startingPoint, numberMachines,
-                       numTraders, row) == -1) {
+        startingPoint = subsecKSum(consecMachineSums, startingPoint, numberMachines,
+                                   numTraders, row);
+        if (startingPoint == -1) {
             return 0;
         }
 
         minTime = consecMachineSums[startingPoint];
+        for (int i = startingPoint + numberMachines; i < row.length; i++) {
 
-        for (int i = startingPoint; i + numberMachines < row.length; i++) {
+            if (row[i].equals("X")) {
 
-            if (row[i + numberMachines].equals("X")) {
-                if (i + numberMachines >= row.length)
+                if (i + numberMachines >= row.length) {
                     return minTime;
+                }
                 else {
-                    i += numberMachines + 1;
-
-                    if (subsecKSum(consecMachineSums,i, numberMachines, numTraders, row) == -1) {
+                    i++;
+                    if ((i = subsecKSum(consecMachineSums, i, numberMachines,
+                                        numTraders, row)) == -1) {
                         return minTime;
                     }
                     newTime = consecMachineSums[i];
+                    consecMachineSums[i - numberMachines + 1] = newTime;
+                    i += numberMachines - 1;
                 }
-                if (newTime < minTime)
-                    minTime = newTime;
+
+                minTime = Math.min(newTime, minTime);
+
             }
+            else {
+                newTime = consecMachineSums[i - numberMachines] - Integer.parseInt(row[i - numberMachines]) +
+                Integer.parseInt(row[i]);
 
-            newTime = consecMachineSums[i] - Integer.parseInt(row[i]) +
-            Integer.parseInt(row[i + numberMachines]);
+                consecMachineSums[i - numberMachines + 1] = newTime;
 
-            consecMachineSums[i + 1] = newTime;
-
-            if (newTime < minTime)
-                minTime = newTime;
+                minTime = Math.min(newTime, minTime);
+            }
         }
 
         return minTime;
@@ -67,27 +76,29 @@ public class Question4 {
 
 
 
-    public static int subsecKSum(int[] consecMachineSums, int startingPoint,
+    public static int subsecKSum(int consecMachineSums[], int startingPoint,
                                  int numberMachines, int numTraders,
                                  String row[]) {
         int numConsec = 0;
-        while (startingPoint <= (numTraders - numberMachines) && numConsec < numberMachines) {
-            for (int i = startingPoint; i < (numberMachines + startingPoint); i++) {
+        int maxRightPos = numTraders - numberMachines;
 
+        while (startingPoint <= maxRightPos && numConsec < numberMachines) {
+
+            for (int i = startingPoint; i < (numberMachines + startingPoint); i++) {
                 if (row[i].equals("X")) {
                     startingPoint++;
                     consecMachineSums[startingPoint] = 0;
+                    numConsec = 0;
                     break;
                 }
-                consecMachineSums[startingPoint] += Integer.parseInt(row[i]);
                 numConsec++;
+                consecMachineSums[startingPoint] += Integer.parseInt(row[i]);
             }
-
         }
 
-        if (startingPoint > (numTraders - numberMachines))
+        if (numConsec != numberMachines)
             return -1;
-        return 0;
+        return startingPoint;
     }
 
 }
