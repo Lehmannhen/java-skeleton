247e058dca96ac69550e491d90d759f50790af4f
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-24 17:25:26 +0100

    First attempt on Question 5

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,10 +1,52 @@
 package answers;
+import java.lang.Math;
 
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
 
+        if (allowedAllocations == null || allowedAllocations.length == 0)
+            return 0;
+
+        boolean existAtLeastOneSolution = false;
+
+        // Scan through to see if there exist a min solution
+        for (int i = 0; i < allowedAllocations.length; i++) {
+            if (allowedAllocations[i] == totalValue)
+                return 1;
+            else if ((totalValue % allowedAllocations[i]) == 0)
+                existAtLeastOneSolution = true;
+        }
+
+        // No solution
+        if (!existAtLeastOneSolution)
+            return 0;
+
+        int[][] dynamicP = new int[allowedAllocations.length + 1][];
+
+        // initialise matrix
+        for (int i = 0; i < dynamicP.length; i++) {
+            dynamicP[i] = new int[totalValue + 1];
+            for (int j = 0; j < totalValue + 1; j++)
+                dynamicP[i][j] = 0;
+        }
+
+        // initialise first row with corresponding sum
+        for (int i = 0; i < totalValue + 1; i++)
+            dynamicP[0][i] = i;
+
+        for (int i = 1; i <= allowedAllocations.length; i++) {
+            for (int j = 1; j <= totalValue; j++) {
+                if (allowedAllocations[i - 1] == j)
+                    dynamicP[i][j] = 1;
+                else if (allowedAllocations[i - 1] > j)
+                    dynamicP[i][j] = dynamicP[i - 1][j];
+                else
+                    dynamicP[i][j] = Math.min(dynamicP[i - 1][j], 1 +
+                                            dynamicP[i][j - allowedAllocations[i - 1]]);
+            }
+        }
+
+        return dynamicP[allowedAllocations.length][totalValue];
+	}
 }
