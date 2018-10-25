7db1012e6893709f606011bc94450512388bfaf5
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-25 23:23:32 +0100

    rewritten function to get rid of previous bugs

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -5,48 +5,30 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 
-        if (allowedAllocations == null || allowedAllocations.length == 0)
+        if (allowedAllocations == null)
             return 0;
 
-        boolean existAtLeastOneSolution = false;
+        int[] dynamicP = new int[totalValue + 1];
 
-        // Scan through to see if there exist a min solution
-        for (int i = 0; i < allowedAllocations.length; i++) {
-            if (allowedAllocations[i] == totalValue)
-                return 1;
-            else if ((totalValue % allowedAllocations[i]) == 0)
-                existAtLeastOneSolution = true;
-        }
+        // No overflow issues
+        int max = Integer.MAX_VALUE - 3;
+        dynamicP[0] = 0;
 
-        // No solution
-        if (!existAtLeastOneSolution)
-            return 0;
+        for (int i = 1; i <= totalValue; i++)
+            dynamicP[i] = max;
 
-        int[][] dynamicP = new int[allowedAllocations.length + 1][];
-
-        // initialise matrix
-        for (int i = 0; i < dynamicP.length; i++) {
-            dynamicP[i] = new int[totalValue + 1];
-            for (int j = 0; j < totalValue + 1; j++)
-                dynamicP[i][j] = 0;
-        }
-
-        // initialise first row with corresponding sum
-        for (int i = 0; i < totalValue + 1; i++)
-            dynamicP[0][i] = i;
-
-        for (int i = 1; i <= allowedAllocations.length; i++) {
-            for (int j = 1; j <= totalValue; j++) {
-                if (allowedAllocations[i - 1] == j)
-                    dynamicP[i][j] = 1;
-                else if (allowedAllocations[i - 1] > j)
-                    dynamicP[i][j] = dynamicP[i - 1][j];
-                else
-                    dynamicP[i][j] = Math.min(dynamicP[i - 1][j], 1 +
-                                            dynamicP[i][j - allowedAllocations[i - 1]]);
+        for (int i = 0; i <= allowedAllocations.length - 1; i++) {
+            for (int k = 1; k <= totalValue; k++) {
+                if (allowedAllocations[i] <= k)
+                    if ((dynamicP[k - allowedAllocations[i]] + 1) < dynamicP[k])
+                        dynamicP[k] = dynamicP[k - allowedAllocations[i]] + 1;
             }
         }
 
-        return dynamicP[allowedAllocations.length][totalValue];
+        if (dynamicP[totalValue] == max)
+            return 0;
+        else
+            return dynamicP[totalValue];
+
 	}
 }
