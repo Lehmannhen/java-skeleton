9ce8529e281637ebf39edf0f278164498ae99e5c
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-28 12:50:24 +0000

    First attempt, not sure if the list of edges is 1-indexed

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -1,12 +1,52 @@
 package answers;
 
+import java.util.HashSet;
 import helpers.Edge;
 
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-		// TODO Auto-generated method stub
-		return -1;
+        HashSet<Integer> exchanges = new HashSet<Integer>();
+        int numTimesVisited[] = new int[numNodes + 1];
+        int numOfTrades = edgeList.length - 1;
+        int edgeA;
+        int edgeB;
+
+        for (int i = 1; i < numTimesVisited.length; i++)
+            numTimesVisited[i] = 0;
+
+        edgeA = edgeList[1].getEdgeA();
+        edgeB = edgeList[1].getEdgeB();
+        exchanges.add(edgeA);
+        exchanges.add(edgeB);
+        numTimesVisited[edgeA]++;
+        numTimesVisited[edgeB]++;
+
+        for (int e = 2; e < edgeList.length; e++) {
+            edgeA = edgeList[e].getEdgeA();
+            edgeB = edgeList[e].getEdgeB();
+            if (exchanges.contains(edgeA)) {
+                numTimesVisited[edgeA]++;
+                if (numTimesVisited[edgeA] == 2)
+                    numOfTrades--;
+            }
+            else {
+                exchanges.add(edgeA);
+                numTimesVisited[edgeA]++;
+            }
+            if (exchanges.contains(edgeB)) {
+                numTimesVisited[edgeB]++;
+                if (numTimesVisited[edgeB] == 2)
+                    numOfTrades--;
+            }
+            else {
+                exchanges.add(edgeB);
+                numTimesVisited[edgeB]++;
+            }
+
+        }
+
+        return numOfTrades;
 	}
 
 }
