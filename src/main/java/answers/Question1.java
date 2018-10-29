7c319e4ab94feaa40f15a575d2464ee366b5fd21
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-29 08:43:45 +0000

    Original solution seems faster for smaller input

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,26 +1,17 @@
 package answers;
-import java.util.HashSet;
+import java.lang.Math;
 
 public class Question1 {
 
     public static int bestMergedPortfolio(int[] portfolios) {
-        int maxPortfolio = 0;
-        int bitMask = 0;
+        int maxPortfolio = -1;
+        int temp;
 
-        for(int k = 31; k >= 0; k--) {
-            bitMask = bitMask | (1 << k);
-            HashSet<Integer> set = new HashSet<>();
-
-            int temp = maxPortfolio | (1 << k);
-            for (int i = 0; i < portfolios.length; i++) {
-                int stockAndMask = portfolios[i] & bitMask;
-                if (set.contains(temp ^ stockAndMask)) {
-                    maxPortfolio = temp;
-                    break;
-                }
-                set.add(stockAndMask);
+        for (int i = 0; i < portfolios.length - 1; i++) {
+            for (int j = i + 1; j < portfolios.length; j++) {
+                temp = portfolios[i] ^ portfolios[j];
+                maxPortfolio = Math.max(maxPortfolio, temp);
             }
-
         }
         return maxPortfolio;
     }
