1293761ecad2fff2264cecb05bd2f66a02b7b100
Henrik Lehmann <lehmannhen@gmail.com>
2018-10-30 21:00:38 +0000

    Trying something else

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -1,50 +1,59 @@
 package answers;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import helpers.Edge;
 
 public class Question3 {
 
     public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-        HashSet<Integer> exchanges = new HashSet<Integer>();
-        int numTimesVisited[] = new int[numNodes + 1];
-        int numOfTrades = edgeList.length;
-        int edgeA;
-        int edgeB;
+        int[] counts = new int[numNodes + 1];
+        int length = counts.length;
+        int numOfTrades = 0;
+        int numOfLeast = 1;
+        int numOfMost = 1;
+        int temp;
+        int i;
 
-        for (int i = 1; i < numTimesVisited.length; i++)
-            numTimesVisited[i] = 0;
+        if (edgeList == null || edgeList.length == 0)
+            return 0;
 
-        edgeA = edgeList[0].getEdgeA();
-        edgeB = edgeList[0].getEdgeB();
-        exchanges.add(edgeA);
-        exchanges.add(edgeB);
-        numTimesVisited[edgeA]++;
-        numTimesVisited[edgeB]++;
+        for (int j = 0; j < length; j++)
+            counts[j] = 0;
 
-        for (int e = 1; e < edgeList.length; e++) {
-            edgeA = edgeList[e].getEdgeA();
-            edgeB = edgeList[e].getEdgeB();
-            if (exchanges.contains(edgeA)) {
-                numTimesVisited[edgeA]++;
-                if (numTimesVisited[edgeA] == 2)
-                    numOfTrades--;
-            }
-            else {
-                exchanges.add(edgeA);
-                numTimesVisited[edgeA]++;
-            }
-            if (exchanges.contains(edgeB)) {
-                numTimesVisited[edgeB]++;
-                if (numTimesVisited[edgeB] == 2)
-                    numOfTrades--;
-            }
-            else {
-                exchanges.add(edgeB);
-                numTimesVisited[edgeB]++;
-            }
-
+        for (Edge e: edgeList) {
+            counts[e.getEdgeA()]++;
+            counts[e.getEdgeB()]++;
         }
+
+        Arrays.sort(counts);
+
+        // Find number of of exchanges with least connections
+        temp = counts[1];
+        i = 2;
+
+        while (i < length) {
+            if (temp == counts[i]) {
+                numOfLeast++;
+            }
+            else
+                break;
+            i++;
+        }
+
+        if (i >= length)
+            return numOfLeast;
+
+        temp = counts[numNodes];
+        for (int j = numNodes - 1; j > 0; j--) {
+            if (temp == counts[j]) {
+                numOfMost++;
+            }
+            else
+                break;
+        }
+
+        numOfTrades = numOfLeast - numOfMost;
+
         return numOfTrades;
 
     }
